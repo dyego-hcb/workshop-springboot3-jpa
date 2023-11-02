b369ba8f5bd2c5850c50418a150213fb719fb88c
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-02 15:53:02 -0300

    Product

---
+++ course/src/main/java/com/zebodexxsz/course/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.zebodexxsz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zebodexxsz.course.entities.Product;
+import com.zebodexxsz.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll()
+	{
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id)
+	{
+		Optional<Product> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+}
